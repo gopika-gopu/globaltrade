1bb736d49373b11bd87c5ac37017e0d78bb13575
Gopika G <gopikag9539@gmail.com>
2020-01-08 19:32:08 +0530

    pagination

--- globaltrade/spiders/us_service_providers.py
+++ globaltrade/spiders/us_service_providers.py
@@ -5,7 +5,7 @@ import scrapy
 class UsServiceProvidersSpider(scrapy.Spider):
     name = 'us_service_providers'
     allowed_domains = ['www.globaltrade.net']
-    start_urls = ['https://www.globaltrade.net/United-States/expert-service-provider.html']
+    start_urls = ['https://www.globaltrade.net/United-States/expert-service-provider.html?pageSize=10&orderBy=1&filterByPost=false&filterByRef=false&topicClear=false&industryClear=false&currentPage=%s'  % page for page in range(1,3)]
 
     def parse(self, response):
         provider_url = response.xpath('//li[@class="sp-id"]/p[@class="sp-name"]/a[@class="profileNavigator"]/@href').extract()
