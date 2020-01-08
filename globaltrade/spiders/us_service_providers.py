6a1201f6fca669a22b65472e8573dd0de3dbf49f
Gopika G <gopikag9539@gmail.com>
2020-01-08 20:01:10 +0530

    detail page urls

--- globaltrade/spiders/us_service_providers.py
+++ globaltrade/spiders/us_service_providers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from urllib.parse import urljoin
 
 
 class UsServiceProvidersSpider(scrapy.Spider):
@@ -8,6 +9,9 @@ class UsServiceProvidersSpider(scrapy.Spider):
     start_urls = ['https://www.globaltrade.net/United-States/expert-service-provider.html?pageSize=10&orderBy=1&filterByPost=false&filterByRef=false&topicClear=false&industryClear=false&currentPage=%s'  % page for page in range(1,3)]
 
     def parse(self, response):
-        provider_url = response.xpath('//li[@class="sp-id"]/p[@class="sp-name"]/a[@class="profileNavigator"]/@href').extract()
-        print(provider_url)
+        provider_urls = response.xpath('//li[@class="sp-id"]/p[@class="sp-name"]/a[@class="profileNavigator"]/@href').extract()
+        for provider in provider_urls:
+            new_page_url  = 'https://www.globaltrade.net' + provider
+            url = urljoin(response.url, new_page_url)
+            print(url)
         pass
